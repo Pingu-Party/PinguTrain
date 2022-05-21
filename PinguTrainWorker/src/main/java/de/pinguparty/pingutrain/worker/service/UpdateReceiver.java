05901a8fd925bc3be35aff7f733931887749db55
Jan Schneider <jan0508@gmx.de>
2022-05-21 20:37:36 +0200

    Update

--- PinguTrainWorker/src/main/java/de/pinguparty/pingutrain/worker/service/UpdateReceiver.java
+++ PinguTrainWorker/src/main/java/de/pinguparty/pingutrain/worker/service/UpdateReceiver.java
@@ -1,12 +1,12 @@
 package de.pinguparty.pingutrain.worker.service;
 
+import de.pinguparty.pingutrain.bot.domain.ReceivedMessage;
+import de.pinguparty.pingutrain.worker.util.JSONConverter;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
 
 @Service
 public class UpdateReceiver {
@@ -19,13 +19,17 @@ public class UpdateReceiver {
 
     @RabbitListener(queues = {"${pingutrain.worker.queue.updates}"})
     public void receiveUpdate(@Payload String payload) {
+        System.out.println("Message " + payload);
+
         //Sanity checks
         if ((payload == null) || payload.isEmpty()) {
             return;
         }
 
+        //Create update message object from payload
+        ReceivedMessage updateMessage = JSONConverter.fromJSON(payload, ReceivedMessage.class);
 
-
-        System.out.println("Message " + payload);
+        //Sanity check
+        if (updateMessage == null) return;
     }
 }
